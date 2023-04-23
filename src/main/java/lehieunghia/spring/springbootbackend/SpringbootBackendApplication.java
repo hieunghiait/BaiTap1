550ae5027a3bc070e3d0cae9f3967031a6478572
Le Hieu Nghia <lehieunghia2402@gmail.com>
2023-04-24 00:56:00 +0700

    add get employee by id specify

--- src/main/java/lehieunghia/spring/springbootbackend/SpringbootBackendApplication.java
+++ src/main/java/lehieunghia/spring/springbootbackend/SpringbootBackendApplication.java
@@ -8,7 +8,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootBackendApplication  implements CommandLineRunner {
+public class SpringbootBackendApplication implements CommandLineRunner {
 
 	public static void main(String[] args)
 	{
--- src/main/java/lehieunghia/spring/springbootbackend/controller/EmployeeController.java
+++ src/main/java/lehieunghia/spring/springbootbackend/controller/EmployeeController.java
@@ -1,21 +1,42 @@
 package lehieunghia.spring.springbootbackend.controller;
 
 import lehieunghia.spring.springbootbackend.model.Employee;
+import lehieunghia.spring.springbootbackend.model.ResourceNotFoundException;
 import lehieunghia.spring.springbootbackend.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+/**
+ * Get API
+ */
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/v1/employees")
 public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
+
     @GetMapping
-    public List<Employee> getAllEmployees(){
+    public List<Employee> getAllEmployees() {
         return employeeRepository.findAll();
+    }
+
+    //build create employee REST API
+    @PostMapping
+    public Employee createEmployee(@RequestBody Employee employee) {
+        return employeeRepository.save(employee);
+    }
+
+    /*
+     * Function get employee by id specify
+     * */
+    @GetMapping("{id}")
+    public ResponseEntity<Employee> getEmployeeById(@PathVariable long id) {
+        Employee employee = employeeRepository.findById(id).orElseThrow(() ->
+                new ResourceNotFoundException("Employee not exits with id: " + id));
+        return ResponseEntity.ok(employee);
     }
 }
