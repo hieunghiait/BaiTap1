7b9b4bb348cd0bb9a0fc8526346d8483dce81938
Le Hieu Nghia <lehieunghia2402@gmail.com>
2023-04-23 23:49:53 +0700

    upload-Method-POST

--- src/main/java/lehieunghia/spring/springbootbackend/SpringbootBackendApplication.java
+++ src/main/java/lehieunghia/spring/springbootbackend/SpringbootBackendApplication.java
@@ -1,13 +1,30 @@
 package lehieunghia.spring.springbootbackend;
 
+import lehieunghia.spring.springbootbackend.model.Employee;
+import lehieunghia.spring.springbootbackend.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootBackendApplication {
+public class SpringbootBackendApplication  implements CommandLineRunner {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		SpringApplication.run(SpringbootBackendApplication.class, args);
 	}
 
+	@Autowired
+	private EmployeeRepository employeeRepository;
+
+	@Override
+	public void run(String... args) throws Exception {
+		Employee employee = new Employee();
+		employee.setId(1);
+		employee.setFirstName("Le");
+		employee.setLastName("Hieu Nghia");
+		employee.setEmailId("lehieunghia2402@gmail.com");
+		employeeRepository.save(employee);
+	}
 }
--- src/main/java/lehieunghia/spring/springbootbackend/model/ResourceNotFoundException.java
+++ src/main/java/lehieunghia/spring/springbootbackend/model/ResourceNotFoundException.java
@@ -1,8 +1,15 @@
 package lehieunghia.spring.springbootbackend.model;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 /**
  *
  */
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(String message){
+        super(message);
 
+    }
 }
